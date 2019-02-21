1db79b0fcb69dc51baa534c29495e5ceaf549e3c
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-02-21 10:13:22 +0100

    equazioni1

---
+++ TeoriaEquazioni/EquazioniLibrary.Test/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EquazioniLibrary.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestIsDetermined()
+        {
+            double a = 3;
+            double b = 0;
+            bool respattesa = true;
+            bool resp = Equazioni.IsDetermired(a,b);
+            Assert.AreEqual(respattesa, resp);
+        }
+        [TestMethod]
+        public void TestIsDetermired1()
+        {
+            double a = 0;
+            double b= 0 ;
+            bool respattesa = false, resp = Equazioni.IsDetermired(a,b );
+            Assert.AreEqual(respattesa, resp);
+        }
+        [TestMethod]
+        public void TestIsDetermired2()
+        {
+            double a = -2;
+            double b = 0;
+            bool respattesa = true, resp = Equazioni.IsDetermired(a, b);
+            Assert.AreEqual(respattesa, resp);
+        }
+        [TestMethod]
+        public void TestIsDetermired3()
+        {
+            double a = 0;
+                double b = 0;
+            bool respattesa = false, resp = Equazioni.IsDetermired(a, b);
+            Assert.AreEqual(respattesa, resp);
+        }
+    }
+}
---
+++ TeoriaEquazioni/EquazioniLibrary/Equazioni.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquazioniLibrary
+{
+    public class Equazioni
+    {
+        public static bool IsDetermired(double a, double b)
+        {
+            bool risp = true;
+            if (a != 0&& b !=0)
+            {
+                risp = false;
+            }
+
+            return risp;
+
+        }
+        public static bool IsImpossible(double b, double a)
+        {
+            bool risp = false;
+            if (b != 0 && a == 0)
+            {
+                risp = true;
+            }
+
+            return risp;
+
+
+
+        }
+    }
+}
